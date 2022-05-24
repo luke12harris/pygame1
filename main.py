9299c34fc308f26911ab12a7a9418a3b6dbb04f0
luke12harris <luke12harris@gmail.com>
2022-05-24 16:45:21 +0100

    initial copy from replit

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame
+
+#initialise the game
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((420, 420))
+
+#name and icon
+icon = pygame.image.load("Big_Chungus_Logo.jpg")
+pygame.display.set_icon(icon)
+pygame.display.set_caption("Not Mario")
+
+#chungus
+chungusImage = pygame.image.load("chungus_character_small.png")
+chungusX = 69
+chungusY = 69
+chungusX_change = 0
+chungusY_change = 0
+
+
+def player(x, y):  #draws chungus
+    screen.blit(chungusImage, (x, y))
+
+
+running = True
+#the game loop
+while running:
+
+    #screen color
+    screen.fill(("blue"))
+
+    for event in pygame.event.get():  #an event is any action within the window
+
+        #quit
+        if event.type == pygame.QUIT:
+            running = False
+
+        #player movement
+        if event.type == pygame.KEYDOWN:  #pressed key
+
+            if event.key == pygame.K_LEFT:
+                chungusX_change = -0.5
+
+            if event.key == pygame.K_RIGHT:
+                chungusX_change = +0.5
+
+            if event.key == pygame.K_UP:
+                chungusY_change = -0.5
+
+            if event.key == pygame.K_DOWN:
+                chungusY_change = +0.5
+
+        if event.type == pygame.KEYUP:  #released key
+            chungusX_change = 0
+            chungusY_change = 0
+
+    chungusX += chungusX_change
+    chungusY += chungusY_change
+
+    if chungusX < 0:
+        chungusX = 0
+
+    if chungusY < 0:
+        chungusY = 5
+
+    if chungusX > 300:
+        chungusX=300
+
+    if chungusY > 300:
+        chungusY = 300
+    player(chungusX, chungusY)  #updates position
+
+    pygame.display.update()
